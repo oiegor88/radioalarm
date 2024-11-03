79f892ec1c4e7b62c0057dabc84c111e6847061a
Oleksandr Iegorov <oleksandri@peoplestrust.com>
2024-11-03 17:51:35 +0200

    ssl stuff

--- src/main/java/io/radioalarm/api/ApiConfiguration.java
+++ src/main/java/io/radioalarm/api/ApiConfiguration.java
@@ -1,22 +1,33 @@
 package io.radioalarm.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
-public class ApiConfiguration implements WebMvcConfigurer {
+public class ApiConfiguration  {
 
-  private final CorsProperties corsProperties;
-
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api/**")
-        .allowedOrigins(corsProperties.getAllowedOrigins())
-        .allowedMethods(corsProperties.getAllowedMethods())
-        .allowedHeaders(corsProperties.getAllowedHeaders())
-        .allowCredentials(corsProperties.isAllowCredentials());
+  @Bean
+  public WebMvcConfigurer corsConfigurer(CorsProperties corsProperties) {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(corsProperties.getAllowedOrigins())
+            .allowedMethods(corsProperties.getAllowedMethods())
+            .allowedHeaders(corsProperties.getAllowedHeaders())
+            .allowCredentials(corsProperties.isAllowCredentials())
+            .exposedHeaders(
+                HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN,
+                HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS
+            )
+        ;
+      }
+    };
   }
 }
